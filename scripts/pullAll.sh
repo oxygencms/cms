c990919fade96eabc5210840d7383e4280384f71
Martin Blagoev <mb.reppair@gmail.com>
2018-11-05 17:38:04 +0200

    add pullAll.sh script to pull all repos

---
+++ scripts/pullAll.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+cd oxygencms/blocks/
+pwd
+git pull
+printf "***********************\n\n"
+
+cd ../commands
+pwd
+git pull
+printf "***********************\n\n"
+
+cd ../core
+pwd
+git pull
+printf "***********************\n\n"
+
+cd ../menus
+pwd
+git pull
+printf "***********************\n\n"
+
+cd ../pages
+pwd
+git pull
+printf "***********************\n\n"
+
+cd ../phrases
+pwd
+git pull
+printf "***********************\n\n"
+
+cd ../uploads
+pwd
+git pull
+printf "***********************\n\n"
+
+cd ../users
+pwd
+git pull
+printf "***********************\n\n"
