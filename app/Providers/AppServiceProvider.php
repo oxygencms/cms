8f1cf2a2951ef8cffdf85f232d04695d0f333f9c
Martin Blagoev <mb.reppair@gmail.com>
2019-02-12 15:10:29 +0200

    udpates to reflect phrases and pages package udpates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use JavaScript;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,30 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->loadPhrasesToJavascript();
+    }
+
+    /**
+     * Load the translations, phrases, locale & locales to Javascript.
+     *
+     * @return void
+     */
+    private function loadPhrasesToJavascript(): void
+    {
+        if (config('phrases.load_to_javascript', false)) {
+            $groups = ['db', 'headings', 'labels', 'buttons', 'links'];
+
+            $translations = [];
+
+            foreach ($groups as $group) {
+                $translations[$group] = app('translation.loader')->load(app()->getLocale(), $group);
+            }
+
+            JavaScript::put([
+                'translations' => $translations,
+                'locale' => $this->app->getLocale(),
+                'locales' => config('oxygen.locales'),
+            ]);
+        }
     }
 }
